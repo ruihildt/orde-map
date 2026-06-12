579dfbcf35617ffa8e56b1742a98376a757211c2
rui hildt <rui@mullvad.net>
2026-06-13 01:22:11 +0200

    Update page info and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import {
 		PUBLIC_UMAMI,
 		PUBLIC_UMAMI_SCRIPT_URL,
@@ -9,12 +8,28 @@
 	import { SidebarProvider, SidebarInset } from '$lib/components/ui/sidebar';
 	import AppSidebar from '$lib/components/AppSidebar.svelte';
 	import TitleBar from '$lib/components/TitleBar.svelte';
+	import { selectedLeague } from '$lib/stores';
+	import { page } from '$app/state';
 
 	let { children } = $props();
+
+	const pageTitles: Record<string, string> = {
+		'/leagues': 'Leagues',
+		'/license': 'License',
+		'/about': 'About'
+	};
+
+	let pageTitle = $derived(
+		page.url.pathname === '/leagues' && $selectedLeague
+			? `${$selectedLeague.name} – Open Roller Derby Europe`
+			: page.url.pathname === '/'
+				? 'Open Roller Derby Europe'
+				: `${pageTitles[page.url.pathname] ?? ''} – Open Roller Derby Europe`
+	);
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>{pageTitle}</title>
 	{#if PUBLIC_UMAMI !== '0' && PUBLIC_UMAMI !== 'false'}
 		<script defer src={PUBLIC_UMAMI_SCRIPT_URL} data-website-id={PUBLIC_UMAMI_WEBSITE_ID}></script>
 	{/if}
